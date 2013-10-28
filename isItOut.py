1e9436cf85b2c7e8dbf702e9fc28afbcb2e01ab0
ljhpauls <ljhpauls@student.cs.uwaterloo.ca>
2013-10-28 17:44:08 -0400

    take credentials as a cmd line arg rather than hard-coded

--- isItOut.py
+++ isItOut.py
@@ -1,22 +1,25 @@
-import urllib2, re, time
+import urllib2, re, time, sys
 import sendEmail
 
 EVENT_STRING = 'nexus 5'
 EVENT_URL = 'https://play.google.com/store/devices'
 
-p = re.compile(EVENT_STRING, re.IGNORECASE)
-pageContents = urllib2.urlopen(EVENT_URL).read()
+def main():
+    p = re.compile(EVENT_STRING, re.IGNORECASE)
+    pageContents = urllib2.urlopen(EVENT_URL).read()
 
-if p.search(pageContents):
-    msg = 'Found "' + EVENT_STRING + \
-        '" in "' + EVENT_URL + '" at ' + \
-        time.asctime(time.gmtime()) + ' UTC'
-    print "message: ", msg
-    sendEmail.sendEmail(
-        'leigh.pauls@gmail.com',
-        'Nexus 5 is OUT!!!',
-        msg,
-        'credentials.txt')
-else:
-    print 'no match :('
+    if p.search(pageContents):
+        msg = 'Found "' + EVENT_STRING + \
+            '" in "' + EVENT_URL + '" at ' + \
+            time.asctime(time.gmtime()) + ' UTC'
+        print "message: ", msg
+        sendEmail.sendEmail(
+            'leigh.pauls@gmail.com',
+            'Nexus 5 is OUT!!!',
+            msg,
+            sys.argv[1])
+    else:
+        print 'no match :('
 
+if __name__=='__main__':
+    main()
